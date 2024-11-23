01be52a075ab6ca9814fd222653c19b6c16f28ea
Ratkov_Daniil <daniilratkov3@gmail.com>
2024-11-23 19:28:24 +0700

    the file script.js redesigned

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
 const buttonPressMe = document.getElementById("Press me");
 const buttonMeToo = document.getElementById("Me too");
 const score = document.getElementsByClassName("score")[0];
+
 let num = 0;
-
 let PressMeFlag = true;
-let MeTooScoreFlag = true;
+let MeTooFlag = true;
 
-function changeColorPressMe() {
-  if (PressMeFlag) {
-    buttonPressMe.style.backgroundColor = "rgb(88, 47, 126)";
-    buttonPressMe.style.color = "rgb(218, 172, 89)";
-    PressMeFlag = false;
+function clickButton(button, flag) {
+  if (flag) {
+    button.style.backgroundColor = "rgb(88, 47, 126)";
+    button.style.color = "rgb(218, 172, 89)";
   } else {
-    buttonPressMe.style.backgroundColor = "rgb(169, 144, 203)";
-    buttonPressMe.style.color = " wheat";
-    PressMeFlag = true;
+    button.style.backgroundColor = "rgb(169, 144, 203)";
+    button.style.color = " wheat";
   }
   num++;
   score.textContent = num;
 }
 
-function changeColorMeToo() {
-  if (MeTooScoreFlag) {
-    buttonMeToo.style.backgroundColor = "rgb(88, 47, 126)";
-    buttonMeToo.style.color = "rgb(218, 172, 89)";
-    MeTooScoreFlag = false;
-  } else {
-    buttonMeToo.style.backgroundColor = "rgb(169, 144, 203)";
-    buttonMeToo.style.color = " wheat";
-    MeTooScoreFlag = true;
-  }
-  num++;
-  score.textContent = num;
+function clickPressMe() {
+  clickButton(buttonPressMe, PressMeFlag);
+  PressMeFlag = !PressMeFlag;
 }
 
-buttonPressMe.addEventListener("click", changeColorPressMe);
-buttonMeToo.addEventListener("click", changeColorMeToo);
+function clickMeToo() {
+  clickButton(buttonMeToo, MeTooFlag);
+  MeTooFlag = !MeTooFlag;
+}
+
+buttonPressMe.addEventListener("click", clickPressMe);
+buttonMeToo.addEventListener("click", clickMeToo);
